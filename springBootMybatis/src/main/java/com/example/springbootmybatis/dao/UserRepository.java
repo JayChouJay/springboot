ca774839a185d39e359973155ac615900deebda3
JayChouJay <1020968571@qq.com>
2023-02-12 17:28:30 +0800

    test add

--- springBootMybatis/src/main/java/com/example/springbootmybatis/dao/UserRepository.java
+++ springBootMybatis/src/main/java/com/example/springbootmybatis/dao/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.springbootmybatis.dao;
 
 
-import com.example.springbootmybatis.bean.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends  in{
 }
---
+++ springBootMybatis/src/main/java/com/example/springbootmybatis/dao/poetryDao.java
@@ -0,0 +1,5 @@
+package com.example.springbootmybatis.dao;
+
+public class poetryDao {
+    private Long id;
+}
