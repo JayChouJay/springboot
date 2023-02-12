712bdd398dcd6f31d93b9d092fb346927f7612d8
JayChouJay <66653441+JayChouJay@users.noreply.github.com>
2023-02-12 18:40:15 +0800

    Update UserRepository.java

--- springBootMybatis/src/main/java/com/example/springbootmybatis/dao/UserRepository.java
+++ springBootMybatis/src/main/java/com/example/springbootmybatis/dao/UserRepository.java
@@ -4,5 +4,5 @@ package com.example.springbootmybatis.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends inp{
+public interface UserRepository extends inplement{
 }
